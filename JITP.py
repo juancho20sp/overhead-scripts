8aa70c9838e1791435265add45353efeca10ea0d
Juan David Murillo <juancho20sp@gmail.com>
2024-11-10 14:15:14 -0500

    Env vars added

--- JITP.py
+++ JITP.py
@@ -1,51 +1,47 @@
-# Script without fleet provisioning
-from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+# Script for JITP
+# from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+from dotenv import load_dotenv
 import boto3
+import os
 
-# Run AWS configure
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
-# unzip awscliv2.zip
-# sudo ./aws/install
-
-# Need the Access key ID and private and region
-
-# Run this script
-
-# Run the IoT Device client
+# Load env vars
+load_dotenv()
 
 # Replace with your endpoint and region
-endpoint = "a396ubkvp0pdet-ats.iot.us-east-1.amazonaws.com"
-region = "us-east-1"
-policy= "basicPolicy"
-thing_type_name="raspberry"
-thing_group_name="raspberries"
+endpoint         = os.environ.get('AWS_IOT_ENDPOINT')
+region           = os.environ.get('AWS_REGION')
+policy           = os.environ.get('AWS_POLICY')
+thing_type_name  = os.environ.get('AWS_THING_TYPE_NAME')
+thing_group_name = os.environ.get('AWS_THING_GROUP_NAME')
 
-# Función para obtener el número de serie de la Raspberry Pi
+# Get the raspberry pi serial number
 def get_serial_number():
     try:
-        # Lee el número de serie de la Raspberry Pi desde /proc/cpuinfo
+        # Read the serial number from /proc/cpuinfo
         with open("/proc/cpuinfo", "r") as f:
             for line in f:
                 if line.startswith("Serial"):
-                    # Extrae el número de serie, eliminando espacios en blanco y la etiqueta 'Serial :'
+                    # Get just the serial name
                     serial_number = line.strip().split(": ")[1]
                     return serial_number
     except FileNotFoundError:
-        print("No se pudo encontrar el archivo /proc/cpuinfo. Asegúrate de estar en una Raspberry Pi.")
+        print("Could not find file /proc/cpuinfo.")
     except Exception as e:
-        print(f"Error al obtener el número de serie: {e}")
+        print(f"Error on getting the serial number: {e}")
 
-# Obtiene el número de serie para usarlo como nombre del Thing
+
 serial_number = get_serial_number()
-if not serial_number:
-    raise Exception("No se pudo obtener el número de serie de la Raspberry Pi.")
 
-thing_name = f"rb-{serial_number}"  # Nombre del Thing usando el serial
+if not serial_number:
+    raise Exception("Could not find raspberry pi's serial number")
+
+# Define the Thing name
+thing_name = f"rb-{serial_number}" 
 
 # Initialize AWS IoT client
 client = boto3.client('iot', region_name=region)
 
-# Create a new certificate for the device - JITP 1 device
+# Create a new certificate for the device - JITP 
 response = client.create_keys_and_certificate(setAsActive=True)
 
 # Save certificate and key on the device
@@ -55,7 +51,7 @@ with open("deviceCert.pem.crt", "w") as cert_file:
 with open("privateKey.pem.key", "w") as key_file:
     key_file.write(response["keyPair"]["PrivateKey"])
 
-# Guarda también el Amazon Root CA 1 para completar el set de certificados
+# Get the Amazon Root CA 1 for completing the set of certificates
 root_ca_url = "https://www.amazontrust.com/repository/AmazonRootCA1.pem"
 root_ca_file = "rootCA.pem"
 try:
@@ -64,60 +60,57 @@ try:
     with open(root_ca_file, "w") as root_file:
         root_file.write(root_ca_data)
 except ImportError:
-    print("Por favor instala el paquete requests con: pip install requests")
+    print("Please install requests package: pip install requests")
 
 # Attach IoT policy to the certificate
 client.attach_policy(
-    policyName=policy, # * / *
+    policyName=policy,
     target=response["certificateArn"]
-) # Juan's raspberry -> attach juan-raspberry (policy)
+)
 
-# Paso 3: Verifica si el Thing Type existe, si no, crea uno
+# Verify if the thing name exists, if not, creates it
 try:
     client.describe_thing_type(thingTypeName=thing_type_name)
-    print(f"El Thing Type '{thing_type_name}' ya existe.")
+    print(f"Thing Type '{thing_type_name}' exists, adding it")
 except client.exceptions.ResourceNotFoundException:
-    print(f"El Thing Type '{thing_type_name}' no existe. Creando...")
+    print(f"Thing Type '{thing_type_name}' does not exists. Creating it...")
     client.create_thing_type(thingTypeName=thing_type_name)
 
 # Register the device as a new IoT Thing
-# thing_name = "overhead-rasperry-" + response["certificateId"]
-# Use serial number for naming
 try:
     client.describe_thing(thingName=thing_name)
 
-    # Uncomment this if we want to update our thing
-    # Obtén todos los certificados (principals) asociados a este Thing
+    # Comment this from HERE if we do not want to override our AWS IoT Thing
     attached_principals = client.list_thing_principals(thingName=thing_name)["principals"]
     
     # Desvincula todos los certificados (principals)
+    # Remove all certificates (principals) of the thing that we want to override
     for principal in attached_principals:
-        print(f"Desvinculando el principal {principal} de '{thing_name}'...")
+        print(f"Removing principal {principal} from '{thing_name}'...")
         client.detach_thing_principal(
             thingName=thing_name,
             principal=principal
         )
-    print(f"Todos los certificados desvinculados de '{thing_name}'.")
+    print(f"All certificates removed from '{thing_name}'.")
     
-
-    print(f"Certificado {response['certificateId']} desactivado.")
-    
-    # Elimina el Thing después de desvincular los certificados y desactivar el certificado
+    # Remove the thing after removing all certificates from it
     client.delete_thing(thingName=thing_name)
-    print(f"El Thing '{thing_name}' ha sido eliminado exitosamente.")
+    
+    print(f"Thing '{thing_name}' has been deleted successfully.")
+    # UNTIL HERE
 
-    print(f"El Thing '{thing_name}' ya existe.")
 
     client.create_thing(
         thingName=thing_name,
-        thingTypeName=thing_type_name  )
+        thingTypeName=thing_type_name  
+    )
 except client.exceptions.ResourceNotFoundException:
-    # Si no existe, lo creamos
-    print(f"Creando el Thing '{thing_name}'...")
+    # If the thing does not exists, create it
+    print(f"Creating thing named '{thing_name}'...")
     client.create_thing(
         thingName=thing_name,
-        thingTypeName=thing_type_name  # Asociar el Thing con el tipo especificado
-)
+        thingTypeName=thing_type_name
+    )
     
 # Attach certificate to thing
 client.attach_thing_principal(
@@ -125,7 +118,7 @@ client.attach_thing_principal(
     principal=response["certificateArn"]
 )
 
-    # Desactiva el certificado antes de eliminar el "Thing"
+# Make sure that the certificate is activate
 client.update_certificate(
     certificateId=response["certificateId"],
     newStatus="ACTIVE"
@@ -133,20 +126,21 @@ client.update_certificate(
 
 # Add thing to group
 try:
-    # Intentar describir el Thing Group para ver si ya existe
+    # Check if group exists
     client.describe_thing_group(thingGroupName=thing_group_name)
-    print(f"El Thing Group '{thing_group_name}' ya existe.")
+    print(f"Thing Group '{thing_group_name}' exists.")
+
 except client.exceptions.ResourceNotFoundException:
-    # Si no existe, lo creamos
-    print(f"El Thing Group '{thing_group_name}' no existe. Creando...")
+    # If group does not exists, create it
+    print(f"Thing Group named '{thing_group_name}' does not exists. Creating it...")
     client.create_thing_group(
         thingGroupName=thing_group_name,
         thingGroupProperties={
-            "thingGroupDescription": "Grupo para Raspberry Pi dispositivos"
+            "thingGroupDescription": "Raspberry Pi Group"
         }
     )
 
-# Paso 6: Agrega el Thing al Thing Group
+# Add the Thing to the Thing Group
 client.add_thing_to_thing_group(
     thingName=thing_name,
     thingGroupName=thing_group_name
